b6f8706656a352938c0c4f59efe8d1af5357fdc0
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-04 18:56:54 +1100

    Implement basic TMNT gif response
    Always return the same gif

--- tmnt-gifs.rb
+++ tmnt-gifs.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'json'
 
 post '/' do
-  status 200
+  # Might want to add a token read from env variable
+  #status 500 unless request['token'] == 'your-token-here'
+
+  # TODO: pick image based on reaction
+  # reaction = request['text']
+
+  content_type :json
+  {
+    attachments: [
+      {
+        image_url: 'https://media.giphy.com/media/10LNj580n9OmiI/giphy.gif'
+      }
+    ]
+  }.to_json
 end
 
 get '/' do
