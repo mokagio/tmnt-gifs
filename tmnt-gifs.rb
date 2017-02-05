fdbaba98d104a10e0568e15eb1e1fcc74d8a0d9f
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-05 20:27:55 +1100

    Refactor how gifs are stored and selected
    This system will simplify the way new gifs can be added.

--- tmnt-gifs.rb
+++ tmnt-gifs.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 require 'json'
 
+require './gifs'
+
 post '/' do
   # Might want to add a token read from env variable
   #status 500 unless request['token'] == 'your-token-here'
 
-  text, image_url = text_and_image(for_reaction: request['text'])
+  text, image_url = text_and_image(request['text'])
 
   content_type :json
   {
@@ -25,43 +27,11 @@ end
 
 private
 
-def text_and_image(for_reaction:)
-  image_url = gif_url_or_nil(for_reaction: for_reaction)
+def text_and_image(reaction)
+  gif = TMNT_GIFS
+    .select { |h| h[:tags].include?(reaction) }
+    .sample
 
-  if image_url.nil?
-    # return an explanation meassage and a random gif
-    return "I don't have a good reaction for \"#{for_reaction}\", so I'm returning a random one.", random_value(gifs)
-  else
-    return '', image_url
-  end
-end
-
-def gif_url_or_nil(for_reaction:)
-  case for_reaction
-  when 'happy'
-    gifs[:happy]
-  when 'high five'
-    gifs[:high_five]
-  when 'scared', 'scary'
-    gifs[:scary]
-  when 'work', 'hard work', 'focus'
-    gifs[:work]
-  else
-    nil
-  end
-end
-
-def gifs
-  {
-    happy: 'https://media.giphy.com/media/mw2Q2CsUaBFIY/giphy.gif',
-    high_five: 'https://media.giphy.com/media/10LNj580n9OmiI/giphy.gif',
-    scary: 'https://media.giphy.com/media/3dcoLqDDjd9pC/giphy.gif',
-    work: 'https://media.giphy.com/media/cFdHXXm5GhJsc/giphy.gif'
-  }
-end
-
-def random_value(hash)
-  # using last because the result of to_a.sample
-  # will be [key, value]
-  hash.to_a.sample.last
+  return "I don't have a good reaction for \"#{reaction}\", so I'm returning a random one.", TMNT_GIFS.sample[:url] if gif.nil?
+  return '', gif[:url]
 end
